997cfddc3771ccc8aac9b962e052292f49ddf3c0
laceratione <laceratione@gmail.com>
2024-08-16 11:53:44 +0500

    Элемент списка обернут в контейнер Card. В базовый класс клетки BaseCell добавлены поля: brush, image

--- app/src/main/java/ru/android/handsapp/model/Cell.kt
+++ app/src/main/java/ru/android/handsapp/model/Cell.kt
@@ -1,31 +1,62 @@
 package ru.android.handsapp.model
 
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import ru.android.handsapp.R
+
 abstract class BaseCell(
     val title: String,
     val subTitle: String,
+    @DrawableRes val image: Int
 ) {
     open val typeCell: TypeCell = TypeCell.Dead
+    abstract val brush: Brush
 }
 
 class CellNewLife(
     title: String = "Жизнь",
     subTitle: String = "Ку-ку!",
-) : BaseCell(title, subTitle) {
+    image: Int = R.drawable.cell_new_life
+) : BaseCell(title, subTitle, image) {
     override val typeCell: TypeCell = TypeCell.NewLife
+    override val brush: Brush
+        get() = Brush.verticalGradient(
+            colors = listOf(
+                Color(0xFFAD00FF),
+                Color(0xFFFFB0E9)
+            )
+        )
 }
 
 class CellLive(
     title: String = "Живая",
-    subTitle: String = "и шевелится!"
-) : BaseCell(title, subTitle) {
+    subTitle: String = "и шевелится!",
+    image: Int = R.drawable.cell_live
+) : BaseCell(title, subTitle, image) {
     override val typeCell: TypeCell = TypeCell.Live
+    override val brush: Brush
+        get() = Brush.verticalGradient(
+            colors = listOf(
+                Color(0xFFFFB800),
+                Color(0xFFFFF7B0)
+            )
+        )
 }
 
 class CellDead(
     title: String = "Мертвая",
-    subTitle: String = "или прикидывается"
-) : BaseCell(title, subTitle) {
+    subTitle: String = "или прикидывается",
+    image: Int = R.drawable.cell_dead
+) : BaseCell(title, subTitle, image) {
     override val typeCell: TypeCell = TypeCell.Dead
+    override val brush: Brush
+        get() = Brush.verticalGradient(
+            colors = listOf(
+                Color(0xFF0D658A),
+                Color(0xFFB0FFB4)
+            )
+        )
 }
 
 enum class TypeCell {
--- app/src/main/java/ru/android/handsapp/presentation/main/MainScreen.kt
+++ app/src/main/java/ru/android/handsapp/presentation/main/MainScreen.kt
@@ -17,12 +17,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,7 +36,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.android.handsapp.R
 import ru.android.handsapp.model.BaseCell
-import ru.android.handsapp.model.TypeCell
 import ru.android.handsapp.ui.theme.roboto
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -107,12 +107,7 @@ fun CellList(cellies: List<BaseCell>, contentPadding: PaddingValues = PaddingVal
         items(cellies) { cell ->
             CellItemList(
                 cell = cell,
-                modifier = Modifier
-                    .padding(bottom = 4.dp)
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(color = MaterialTheme.colorScheme.primaryContainer)
-                    .padding(16.dp)
+                modifier = Modifier.padding(bottom = 4.dp)
             )
         }
     }
@@ -120,69 +115,50 @@ fun CellList(cellies: List<BaseCell>, contentPadding: PaddingValues = PaddingVal
 
 @Composable
 fun CellItemList(cell: BaseCell, modifier: Modifier = Modifier) {
-    Row(
+    Card(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        ),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .clip(CircleShape)
-                .background(
-                    brush = when (cell.typeCell) {
-                        TypeCell.Live -> Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFFFFB800),
-                                Color(0xFFFFF7B0)
-                            )
-                        )
-
-                        TypeCell.Dead -> Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFF0D658A),
-                                Color(0xFFB0FFB4)
-                            )
-                        )
-
-                        TypeCell.NewLife -> Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFFAD00FF),
-                                Color(0xFFFFB0E9)
-                            )
-                        )
-                    }
-                )
-                .size(40.dp),
-            contentAlignment = Alignment.Center
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
+            Box(
                 modifier = Modifier
-                    .height(40.dp)
-                    .width(21.dp),
-                painter = painterResource(
-                    id = when (cell.typeCell) {
-                        TypeCell.Dead -> R.drawable.cell_dead
-                        TypeCell.Live -> R.drawable.cell_live
-                        TypeCell.NewLife -> R.drawable.cell_new_life
-                    }
-                ),
-                contentDescription = ""
-            )
-        }
-        Column(modifier = Modifier.padding(start = 16.dp)) {
-            Text(
-                text = cell.title,
-                fontFamily = roboto,
-                fontWeight = FontWeight.Medium,
-                fontSize = 20.sp,
-                color = Color.Black
-            )
-            Text(
-                text = cell.subTitle,
-                fontFamily = roboto,
-                fontWeight = FontWeight.Normal,
-                fontSize = 14.sp,
-                color = Color.Black
-            )
+                    .clip(CircleShape)
+                    .background(brush = cell.brush)
+                    .size(40.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(21.dp),
+                    painter = painterResource(id = cell.image),
+                    contentDescription = ""
+                )
+            }
+            Column(modifier = Modifier.padding(start = 16.dp)) {
+                Text(
+                    text = cell.title,
+                    fontFamily = roboto,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+                Text(
+                    text = cell.subTitle,
+                    fontFamily = roboto,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+            }
         }
     }
 }
